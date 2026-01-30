abc2a3d7be30ddc958b3533c4b37f9b8d2b81c1d
ahmniab <ahmniab11@gmail.com>
2026-01-30 21:11:48 +0200

    Feat: Init Blazor

--- CoreSoftware/CoreSoftware/Program.cs
+++ CoreSoftware/CoreSoftware/Program.cs
@@ -1,21 +1,44 @@
 using CoreSoftware.PanTompkins;
 using CoreSoftware.DataStream;
+using CoreSoftware.DataProcessing;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
 var app = builder.Build();
-int last = -1;
-RDetector rd = new(360d);
+app.MapRazorPages();
+app.MapBlazorHub();
+app.MapStaticAssets();
+app.UseStaticFiles();
+
+string textCsv = String.Empty;
+for (int i = 1; i < 93; i++)
+{
+    textCsv += $"sample_{i},";
+}
+
+textCsv += "rr_interval\n";
+
+BeatProducer br = new BeatProducer(360d);
+br.NewBeatProduced += (object? o, NewBeatProducedEventArgs e) =>
+{
+    foreach (var s in e.Beat.Samples)
+    {
+        textCsv += $"{s},";
+    }
+
+    textCsv += $"{e.Beat.RrInterval}\n";
+
+};
+
 ProducerConsumer.ReadFile("Tests/100_raw");
 ProducerConsumer.Start((s) =>
 { 
-    int rIndex = rd.ProcessSample(s);
-    if (rIndex != -1)
-    {
-        last = rIndex;
-    }
+    br.ProcessSample(s);
 });
 
 
-app.MapGet("/", () => $"Last r index = {last}\n");
-
+app.MapGet("/", () => textCsv);
+app.MapFallbackToPage("/EcgStream/{*catchall}", "/EcgStream/Main");
 app.Run();
