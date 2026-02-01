2d8e5e4d8a9e41eb80a212413ac1225be0be68c1
ahmniab <ahmniab11@gmail.com>
2026-02-01 18:29:33 +0200

    fix: init blazor

--- CoreSoftware/CoreSoftware/Program.cs
+++ CoreSoftware/CoreSoftware/Program.cs
@@ -1,44 +1,12 @@
-using CoreSoftware.PanTompkins;
-using CoreSoftware.DataStream;
-using CoreSoftware.DataProcessing;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-
 var app = builder.Build();
-app.MapRazorPages();
-app.MapBlazorHub();
-app.MapStaticAssets();
+
 app.UseStaticFiles();
+app.UseRouting();
+app.MapBlazorHub();
+app.MapFallbackToPage("/{*catchall}", "/EcgStream/Main");
 
-string textCsv = String.Empty;
-for (int i = 1; i < 93; i++)
-{
-    textCsv += $"sample_{i},";
-}
-
-textCsv += "rr_interval\n";
-
-BeatProducer br = new BeatProducer(360d);
-br.NewBeatProduced += (object? o, NewBeatProducedEventArgs e) =>
-{
-    foreach (var s in e.Beat.Samples)
-    {
-        textCsv += $"{s},";
-    }
-
-    textCsv += $"{e.Beat.RrInterval}\n";
-
-};
-
-ProducerConsumer.ReadFile("Tests/100_raw");
-ProducerConsumer.Start((s) =>
-{ 
-    br.ProcessSample(s);
-});
-
-
-app.MapGet("/", () => textCsv);
-app.MapFallbackToPage("/EcgStream/{*catchall}", "/EcgStream/Main");
 app.Run();
